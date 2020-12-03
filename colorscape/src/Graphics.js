b2decc7a06777884e1d60acfc3b55554028e6ab2
Sam Herwif <Sahe2032@colorado.edu>
2020-12-03 13:26:06 -0700

    Update Graphics.js

--- colorscape/src/Graphics.js
+++ colorscape/src/Graphics.js
@@ -11,10 +11,8 @@ class Graphics extends React.Component{
   constructor(props){
 	super(props);
 	this.myRef = React.createRef()
-  this.state={
-			// for state, we will need values for each of the question answers (taken from form via props?), as well as toggles
-			// for the graphics, which will need to turn on/off and change with the form.
-		}
+  // this.state={
+	// 	}
   };
 
 // basic setup for p5, can be used to greater extents once we have the form done
@@ -33,12 +31,21 @@ class Graphics extends React.Component{
 
 		}
 
+
+
 		p.draw = () => {
 
     var name=this.props.answer;
     b=p.map(name.length,0, 30, 0.001, 0.1);
 
     starSign=this.props.starSign;
+
+    p.background(this.props.bgColor);
+    p.fill(255);
+    p.translate(p.width/2,p.height/2);
+    p.rotate(a / 3.0);
+    p.rect(0,0,50,50);
+    p.rect(x,y,50,50);
 
     if (this.props.starSign=="Scorpio")
     {
@@ -47,91 +54,76 @@ class Graphics extends React.Component{
       p.rect(x+50,y+50,50,50);
     }
 
-    // p.elif (starSign=="Capricorn")
-    // {
-    //   {console.log(starSign)}
-    //   p.rect(x+50,y+50,50,50);
-    //
-    // }
+    else if (starSign=="Capricorn")
+    {
+      {console.log(starSign)}
+      p.rect(x+50,y+50,50,50);
 
-    // p.else if (starSign=="Aries")
-    // {
-    //   {console.log(starSign)}
-    // }
-    //
-    // p.else if (starSign=="Libra")
-    // {
-    //   {console.log(starSign)}
-    // }
-    //
-    // p.else if (starSign=="Gemini")
-    // {
-    //   {console.log(starSign)}
-    // }
-    //
-    // p.else if (starSign=="Pisces")
-    // {
-    //   {console.log(starSign)}
-    // }
-    //
-    // p.else if (starSign=="Taurus")
-    // {
-    //   {console.log(starSign)}
-    // }
-    //
-    // p.else if (starSign=="Virgo")
-    // {
-    //   {console.log(starSign)}
-    // }
-    //
-    // p.else if (starSign=="Aquarius")
-    // {
-    //   {console.log(starSign)}
-    // }
-    //
-    // p.else if (starSign=="Sagittarius")
-    // {
-    //   {console.log(starSign)}
-    // }
-    //
-    // p.else if (starSign=="Cancer")
-    // {
-    //   {console.log(starSign)}
-    // }
-    //
-    // p.else if (starSign=="Aquarius")
-    // {
-    //   {console.log(starSign)}
-    // }
+    }
 
-		p.background(this.props.bgColor);
+    else if (starSign=="Aries")
+    {
+      {console.log(starSign)}
+    }
 
-    p.fill(255);
-    // p.push();
-    p.translate(p.width/2,p.height/2);
-    p.rotate(a / 3.0);
-    p.rect(0,0,50,50);
-    p.rect(x,y,50,50);
+    else if (starSign=="Libra")
+    {
+      {console.log(starSign)}
+    }
 
-    // p.rect(x+200,y+200,50,50);
-    // p.rect(x+50,y+50,50,50);
-    // p.pop();
-//   p.rect(-26, -26, 52, 52);
+    else if (starSign=="Gemini")
+    {
+      {console.log(starSign)}
+    }
+
+    else if (starSign=="Pisces")
+    {
+      {console.log(starSign)}
+    }
+
+    else if (starSign=="Taurus")
+    {
+      {console.log(starSign)}
+    }
+
+    else if (starSign=="Virgo")
+    {
+      {console.log(starSign)}
+    }
+
+    else if (starSign=="Aquarius")
+    {
+      {console.log(starSign)}
+    }
+
+    else if (starSign=="Sagittarius")
+    {
+      {console.log(starSign)}
+    }
+
+    else if (starSign=="Cancer")
+    {
+      {console.log(starSign)}
+    }
+
+    else if (starSign=="Aquarius")
+    {
+      {console.log(starSign)}
+    }
+
     a = a + b
 
 		}
 	}
 
 	componentDidMount(){
-		this.myP5 = new p5(this.Sketch, this.myRef.current);
+		this.p = new p5(this.Sketch, this.myRef.current);
+     // this.myP5 = new p5(this.Sketch, this.myRef.current)
 	}
 
 	// code snippet (mostly) via: https://dev.to/christiankastner/integrating-p5-js-with-react-i0d
   render(){
-    // {console.log(this.props.bgColor)}
-    // {console.log(this.props.answer)}
-    {console.log(this.props.starSign)}
-
+    // {console.log(this.props.starSign)}
     return(
 			<div ref = {this.myRef}></div>
 
